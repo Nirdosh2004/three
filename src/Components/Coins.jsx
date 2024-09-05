b3b30620f84627625140cb0fe0bfd93d19490edf
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-09-05 14:57:51 +0530

    5:27:03

--- src/Components/Coins.jsx
+++ src/Components/Coins.jsx
@@ -21,6 +21,8 @@ const Coins = () => {
     setLoading(true);
   }
 
+  const btns = new Array(132).fill(1)
+
   useEffect(()=>{
 
     const fetchCoins = async ()=>{
@@ -51,8 +53,18 @@ const Coins = () => {
       ))}
      </HStack>
 
-     <HStack>
-       <Button bgColor={'blackAlpha.900'} color={'white'} onClick={()=>changePage(2)}>2</Button>
+     <HStack w={'full'} overflowX={'auto'} p={'8'}>
+       {
+        btns.map((item,index)=>(
+          <Button 
+          bgColor={'blackAlpha.900'}
+          color={'white'}
+          onClick={()=>changePage(index+1)}
+          >
+           {index+1}
+          </Button>
+        ))
+       }
      </HStack>
      </>
     )}
